889908b3d76411be68ed85fa49ab93e5c3f6cd60
Ferdinand <wuhi@nod.at>
2011-03-31 08:59:40 +0200

    renamed pwn.ResizeHandle to dojox.layout.CardinalResizeHandle

--- dojox/layout/ResizeHandle.js
+++ dojox/layout/ResizeHandle.js
@@ -1,26 +1,61 @@
-dojo.provide("pwn.ResizeHandle");
+dojo.provide("dojox.layout.CardinalResizeHandle");
 
-dojo.require("dijit._Widget");
-
-dojo.declare("pwn.ResizeHandle", dijit._Widget, {
-    targetNode: null,
+dojo.declare("dojox.layout.CardinalResizeHandle", dijit._Widget, {
+    // summary:
+    //        A class to create a resizehandle based on the cardinal system
+	//		  example http://jsfiddle.net/fCD7W/8/
+    // targetId:
+    //        id of the Widget OR DomNode that I will size
+    targetId: "",
     _handleNodes: [],
-    
+    _pconnects: [],
+
+    constructor: function() {
+        this._pconnects = [];
+        this._handleNodes = [];
+    },
     postCreate: function() {
-        if (dojo.isString(this.targetNode)) {
-            this.targetNode = dojo.byId(this.targetNode);
+        // summary:
+        //            Called after a widget's dom has been setup
+        // tags:
+        //            protected
+        this.inherited(arguments);
+
+        // acceppt targetNode or targetId
+        if (dojo.isString(this.targetId)) {
+            var widget = dijit.byId(this.targetId);
+            var node = dojo.byId(this.targetId);
+            if (widget) {
+                this.targetNode = widget.domNode;
+            } else {
+                this.targetNode = node;
+            }
+        } else {
+            if (this.targetId instanceof(dijit._Widget)) {
+                this.targetNode = this.targetId.domNode;
+            } else {
+                this.targetNode = this.targetId;
+            }
         }
-		
-		this._pconnects = []; 
+        // connect to global mouse events
         this._pconnects.push(dojo.connect(dojo.doc, "onmouseup", this, "_onMouseUp"));
         this._pconnects.push(dojo.connect(dojo.doc, "onmousemove", this, "_onMouseMove"));
 
-        this.createResizeHandles();
+        this._createResizeHandles();
     },
+    // resizeAxis:
+    //        one of: x|y|xy limit resizing to a single axis, default to xy ...
     resizeAxis: "xy",
+    // basezIndex:
+    //        z-index to control the z-index of the resize-squares
     basezIndex: 0,
-	minWidth: 0,
-	minHeight: 0,
+    // minHeight: Integer
+    //    smallest height in px resized node can be
+    minHeight: 25,
+
+    // minWidth: Integer
+    //    smallest width in px resize node can be
+    minWidth: 25,
     _mouseDown: false,
     _lastDirection: "",
     _lastMarginBox: {
@@ -39,13 +74,25 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
         x: 0,
         y: 0
     },
-    onResizeComplete: function(marginBox) {
-        //connect here
-    },
-    onResize: function(node, marginBox){
-    	//connect here
-    },
+    onResizeComplete: function(/*Object?*/marginBox) {
+        // summary:
+        //        connect or override this method to know when the resize is complete
+        // marginBox:
+        //        the dojo.marginBox of the resized node
+        },
+    onResize: function( /*DomNode?*/node, /*Object?*/marginBox) {
+        // summary:
+        //        connect or override this method to know when the node is resizing
+        // node:
+        //        the resize domNode
+        // marginBox:
+        //        the dojo.marginBox of the resized node
+        },
     _onMouseDown: function(e) {
+        // summary:
+        //        Initials the resize
+        // tags:
+        //        protected
         this._lastMarginBox = dojo.marginBox(this.targetNode);
         this._lastPosition = dojo.position(this.targetNode);
         this._lastGrab = {
@@ -62,7 +109,10 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
         dojo.stopEvent(e);
     },
     _onMouseUp: function(e) {
-
+        // summary:
+        //        Stops the resize onMouseUp
+        // tags:
+        //        protected
         if (this._mouseDown == true) {
             this.onResizeComplete(dojo.marginBox(this.targetNode));
         }
@@ -70,18 +120,19 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
         dojo.stopEvent(e);
     },
     _onMouseMove: function(e) {
+        // summary:
+        //        Calculates the new size while a resize is in progress
+        // tags:
+        //        protected
         if (this._mouseDown === true) {
-
             var newBox = {};
 
             if (this._lastDirection.indexOf("E") != -1) {
                 newBox.w = Math.max(this.minWidth, this._lastMarginBox.w + e.clientX - this._lastGrab.x);
             }
-
             if (this._lastDirection.indexOf("S") != -1) {
                 newBox.h = Math.max(this.minHeight, this._lastMarginBox.h + e.clientY - this._lastGrab.y);
             }
-
             if (this._lastDirection.indexOf("W") != -1) {
                 newBox.l = Math.min(this._lastMarginBox.l + e.clientX - this._lastGrab.x, this._lastMarginBox.l + this._lastMarginBox.w - this.minWidth);
                 newBox.w = Math.max(this.minWidth, this._lastMarginBox.w - e.clientX + this._lastGrab.x);
@@ -93,87 +144,102 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
 
             //apply the new size
             dojo.marginBox(this.targetNode, newBox);
-			this.onResize(this.targetNode, newBox);
-			
-            this.createResizeHandles();
+            this.onResize(this.targetNode, newBox);
+
+            this._createResizeHandles();
 
             dojo.stopEvent(e);
         }
     },
-    createResizeHandles: function() {
-
+    _createResizeHandles: function() {
+        // summary:
+        //        Creates new or updates existing squares based on the
+        //        cardinal system to provide the resize handles
+        // tags:
+        //        protected
         var squareBorderSize = 1;
-        var squareSize = 6; //px
-        var squareSizeHalf = 4; //@TODO: fix this in opera. opera needs a fixed value. calc with (squareSize / 2) + squareBorderSize; does not work
+        var squareSize = 6;
+        //px
+        var squareSizeHalf = 4;
         var squareSizePx = squareSize + "px";
         var squareSizeHalfPx = squareSizeHalf + "px";
         var squareSizeHalfNegPx = "-" + squareSizeHalf + "px";
 
+        // descriptor for the resizehandles
         var positions = [{
             "direction": "NW",
             "axis": "xy",
             "style": {
                 "top": squareSizeHalfNegPx,
                 "left": squareSizeHalfNegPx
-            }},
+            }
+        },
         {
             "direction": "NE",
             "axis": "xy",
             "style": {
                 "top": squareSizeHalfNegPx,
                 "right": squareSizeHalfNegPx
-            }},
+            }
+        },
         {
             "direction": "SE",
             "axis": "xy",
             "style": {
                 "bottom": squareSizeHalfNegPx,
                 "right": squareSizeHalfNegPx
-            }},
+            }
+        },
         {
             "direction": "SW",
             "axis": "xy",
             "style": {
                 "bottom": squareSizeHalfNegPx,
                 "left": squareSizeHalfNegPx
-            }},
+            }
+        },
         {
             "direction": "N",
             "axis": "y",
             "style": {
                 "top": squareSizeHalfNegPx,
                 "left": ((dojo.marginBox(this.targetNode).w / 2) - squareSizeHalf) + "px"
-            }},
+            }
+        },
         {
             "direction": "S",
             "axis": "y",
             "style": {
                 "bottom": squareSizeHalfNegPx,
                 "left": ((dojo.marginBox(this.targetNode).w / 2) - squareSizeHalf) + "px"
-            }},
+            }
+        },
         {
             "direction": "W",
             "axis": "x",
             "style": {
                 "left": squareSizeHalfNegPx,
                 "top": ((dojo.marginBox(this.targetNode).h / 2) - squareSizeHalf) + "px"
-            }},
+            }
+        },
         {
             "direction": "E",
             "axis": "x",
             "style": {
                 "right": squareSizeHalfNegPx,
                 "top": ((dojo.marginBox(this.targetNode).h / 2) - squareSizeHalf) + "px"
-            }}];
+            }
+        }];
+        // create the resize handles based on the position descriptor
+        dojo.forEach(positions,
+        function(item) {
+            if (this.resizeAxis.indexOf(item.axis) == -1) {
+                return;
+            }
 
-        dojo.forEach(positions, function(item) {
-        	if(this.resizeAxis.indexOf(item.axis) == -1) { return; }
-        	
             var node;
 
-            if (node = dojo.query("." + "pwnResizeSquare_" + item.direction, this.targetNode)[0]) {
-                //console.log("use existing");
-            } else {
+            if (! (node = dojo.query("." + "dojoxLayoutCardinalResizeSquare_" + item.direction, this.targetNode)[0])) {
                 node = dojo.create("div", {
                     "style": {
                         "position": "absolute",
@@ -185,7 +251,7 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
                     }
                 });
 
-                dojo.addClass(node, "pwnResizeSquare_" + item.direction);
+                dojo.addClass(node, "dojoxLayoutCardinalResizeSquare_" + item.direction);
                 dojo.style(node, "cursor", item.direction + "-resize");
                 dojo.place(node, this.targetNode);
                 this.connect(node, "onmousedown", dojo.hitch(this, "_onMouseDown"));
@@ -193,19 +259,18 @@ dojo.declare("pwn.ResizeHandle", dijit._Widget, {
             }
             //apply the new position to the square
             dojo.style(node, item.style);
-        }, this);
-    },
-    moveTo:function(marginBox){
-    	dojo.marginBox(this.targetNode, marginBox);	
-    	dojo.marginBox(this._outlineBorder, marginBox);
+        },
+        this);
     },
     destroy: function() {
         this.inherited("destroy", arguments);
-		//disconnect listeners
-		dojo.forEach(this._pconnects, dojo.disconnect);
-		//destroy handleNodes
-        dojo.forEach(this._handleNodes, function(node) {
+        //disconnect listeners
+        dojo.forEach(this._pconnects, dojo.disconnect);
+        //destroy handleNodes
+        dojo.forEach(this._handleNodes,
+        function(node) {
             dojo.destroy(node);
-        }, this);
+        },
+        this);
     }
 });
